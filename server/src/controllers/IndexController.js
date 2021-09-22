99a649d3c019a1cd95a6582ad27c106da22c39bd
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2021-09-22 11:47:25 -0300

    [SERVER] feat: Index delete

--- server/src/controllers/IndexController.js
+++
@@ -1,8 +0,0 @@
-// exports a JSON object with routes' actions
-module.exports = {
-	async index(req, res) {
-		return res.json({
-			host: req.hostname
-		})
-	}
-}
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,10 +1,8 @@
 const router = require('express')()
 
 // import controllers
-const IndexController = require('./controllers/IndexController')
 
 // setup URIs (paths)
-router.get('/', IndexController.index)
 
 // export router with paths
 module.exports = router
