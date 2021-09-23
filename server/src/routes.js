9964a6e5dfacf44f6e0418ff312f1362365961ab
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2021-09-23 15:21:12 -0300

    [SERVER] refactor: body parameters
    the parameters check list now is stored in a variable

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,82 +4,47 @@ const { body } = require('express-validator')
 // import controllers
 const ngoController = require('./controllers/NgoController')
 
+// parameter requirements
+const bodyParams = [
+	body('name')
+		.isString()
+		.isLength({
+			min: 4,
+			max: 32
+		}),
+	body('email')
+		.isEmail()
+		.isLength({
+			max: 32
+		}),
+	body('password')
+		.isString()
+		.isLength({
+			min: 8, max: 32
+		}),
+	body('whatsapp')
+		.isMobilePhone()
+		.isNumeric()
+		.isLength({
+			max: 16
+		}),
+	body('city')
+		.isString()
+		.isLength({
+			min: 2,
+			max: 32
+		}),
+	body('state')
+		.isString()
+		.isAlpha()
+		.isLength(2)
+]
+
 // setup URIs (paths)
-router.post('/ngo',
-	[
-		body('name')
-			.isString()
-			.isLength({
-				min: 4,
-				max: 32
-			}),
-		body('email')
-			.isEmail()
-			.isLength({
-				max: 32
-			}),
-		body('password')
-			.isString()
-			.isLength({
-				min: 8,
-				max: 32
-			}),
-		body('whatsapp')
-			.isMobilePhone()
-			.isNumeric()
-			.isLength({
-				max: 16
-			}),
-		body('city')
-			.isString()
-			.isLength({
-				min: 2,
-				max: 32
-			}),
-		body('state')
-			.isString()
-			.isAlpha()
-			.isLength(2)
-	], ngoController.create
-)
+router.post('/ngo', bodyParams, ngoController.create)
 router.get('/ngos', ngoController.index)
 router.get('/ngo/:id', ngoController.show)
-router.patch('/ngo',
-	[
-		body('name')
-			.isString()
-			.isLength({
-				min: 4,
-				max: 32
-			}),
-		body('email')
-			.isEmail()
-			.isLength({
-				max: 32
-			}),
-		body('password')
-			.isString()
-			.isLength({
-				min: 8,
-				max: 32
-			}),
-		body('whatsapp')
-			.isMobilePhone()
-			.isNumeric()
-			.isLength({
-				max: 16
-			}),
-		body('city')
-			.isString()
-			.isLength({
-				min: 2,
-				max: 32
-			}),
-		body('state')
-			.isString()
-			.isAlpha()
-			.isLength(2)
-	], ngoController.update)
+router.patch('/ngo', bodyParams, ngoController.update)
 
 // export router with paths
 module.exports = router
