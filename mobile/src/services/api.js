9ef9371f4cc142d2b0c5c5024b9e641faa76fcf2
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2021-10-08 11:41:53 -0300

    [MOBILE] chore: setup environment variables

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,9 +1,12 @@
 // Axios is HTTP Client which can communicate with the backend
 import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config() // set .env variables
 
 const api = axios.create({
     // IMPORTANT: baseURL value must be your computer's IP with the port
-    baseURL: '' // EXAMPLE: 'http://192.168.1.118:3333'
+    baseURL: BASE_URL // EXAMPLE: 'http://192.168.1.118:3333'
 })
 
 export default api
