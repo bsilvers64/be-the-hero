8405e7fb61751ae23593c9305a1d81ae438881ed
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2021-10-11 12:12:55 -0300

    [WEB] refactor: allow urls to access cors

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const routes = require('./routes')
 
 // setting up routes
 const app = express() // create app with routes' structures
-app.use(cors())  // control access permissions in URLs
+app.use(cors({ origin: '*' }))  // control access permissions in URLs
 app.use(logger('dev')) // create middleware log
 app.use(express.json()) // add JSON conversor
 app.use(express.urlencoded({ extended: false })) // use query string
