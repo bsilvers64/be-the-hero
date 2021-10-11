0b110b9b5c249b12465ce0fca8a22db4567a6973
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2021-10-11 13:29:56 -0300

    [MOBILE] refactor: set an URL for example

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const api = axios.create({
     // IMPORTANT: baseURL value must be your computer's IP with the port
-    baseURL: 'http://192.168.1.102:3333', // EXAMPLE: 'http://192.168.1.118:3333'
+    baseURL: 'http://192.168.1.102:3333', // IMPORTANT: UPDATE THE URL!!
     timeout: 1000 * 30, // wait for 60s
 })
 
